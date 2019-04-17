5b3809ad99079da71d00c64fcc3aef2217cfbb19
marshallprotsman <47481879+MarshallProtsman@users.noreply.github.com>
2019-04-17 16:40:34 -0400

    date-picker (1.1)

--- assets/js/main.js
+++ assets/js/main.js
@@ -8,14 +8,23 @@ $(document).ready(function () {
   /* ----------------------------------------------------------------------------------- */
   /* -------------------------------Date Picker----------------------------------------- */
   /* ----------------------------------------------------------------------------------- */
+let ourDate = '';
   // document.addEventListener('DOMContentLoaded', function() {
   //   var elems = document.querySelectorAll('.datepicker');
   //   var instances = M.Datepicker.init(elems, options);
   // });
-
   // Or with jQuery
-    $('.datepicker').datepicker();
+  $('.datepicker').datepicker({
+    disableWeekends: true,
+    onClose: function() {
+      console.log(date.value);
+      window.ourDate = date.value;;
+    },
+  });
 
+
+
+console.log(ourDate);
 
 
   /* ----------------------------------------------------------------------------------- */
