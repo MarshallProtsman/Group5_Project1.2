3e2d0a30309fbcfe858091ff629f1b0b228b53a3
John Robertson <tiktaalikdesign@gmail.com>
2019-04-19 03:45:01 -0400

    almost there

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,4 +1,7 @@
+let eventsResponse = '';
+
 $(document).ready(function () {
+
   let cityList = ["Aberdeen", "Abilene", "Akron", "Albany", "Albuquerque", "Alexandria", "Allentown", "Amarillo", "Anaheim", "Anchorage", "Ann Arbor", "Antioch", "Apple Valley", "Appleton", "Arlington", "Arvada", "Asheville", "Athens", "Atlanta", "Atlantic City", "Augusta", "Aurora", "Austin", "Bakersfield", "Baltimore", "Barnstable", "Baton Rouge", "Beaumont", "Bel Air", "Bellevue", "Berkeley", "Bethlehem", "Billings", "Birmingham", "Bloomington", "Boise", "Boise City", "Bonita Springs", "Boston", "Boulder", "Bradenton", "Bremerton", "Bridgeport", "Brighton", "Brownsville", "Bryan", "Buffalo", "Burbank", "Burlington", "Cambridge", "Canton", "Cape Coral", "Carrollton", "Cary", "Cathedral City", "Cedar Rapids", "Champaign", "Chandler", "Charleston", "Charlotte", "Chattanooga", "Chesapeake", "Chicago", "Chula Vista", "Cincinnati", "Clarke County", "Clarksville", "Clearwater", "Cleveland", "College Station", "Colorado Springs", "Columbia", "Columbus", "Concord", "Coral Springs", "Corona", "Corpus Christi", "Costa Mesa", "Dallas", "Daly City", "Danbury", "Davenport", "Davidson County", "Dayton", "Daytona Beach", "Deltona", "Denton", "Denver", "Des Moines", "Detroit", "Downey", "Duluth", "Durham", "El Monte", "El Paso", "Elizabeth", "Elk Grove", "Elkhart", "Erie", "Escondido", "Eugene", "Evansville", "Fairfield", "Fargo", "Fayetteville", "Fitchburg", "Flint", "Fontana", "Fort Collins", "Fort Lauderdale", "Fort Smith", "Fort Walton Beach", "Fort Wayne", "Fort Worth", "Frederick", "Fremont", "Fresno", "Fullerton", "Gainesville", "Garden Grove", "Garland", "Gastonia", "Gilbert", "Glendale", "Grand Prairie", "Grand Rapids", "Grayslake", "Green Bay", "GreenBay", "Greensboro", "Greenville", "Gulfport-Biloxi", "Hagerstown", "Hampton", "Harlingen", "Harrisburg", "Hartford", "Havre de Grace", "Hayward", "Hemet", "Henderson", "Hesperia", "Hialeah", "Hickory", "High Point", "Hollywood", "Honolulu", "Houma", "Houston", "Howell", "Huntington", "Huntington Beach", "Huntsville", "Independence", "Indianapolis", "Inglewood", "Irvine", "Irving", "Jackson", "Jacksonville", "Jefferson", "Jersey City", "Johnson City", "Joliet", "Kailua", "Kalamazoo", "Kaneohe", "Kansas City", "Kennewick", "Kenosha", "Killeen", "Kissimmee", "Knoxville", "Lacey", "Lafayette", "Lake Charles", "Lakeland", "Lakewood", "Lancaster", "Lansing", "Laredo", "Las Cruces", "Las Vegas", "Layton", "Leominster", "Lewisville", "Lexington", "Lincoln", "Little Rock", "Long Beach", "Lorain", "Los Angeles", "Louisville", "Lowell", "Lubbock", "Macon", "Madison", "Manchester", "Marina", "Marysville", "McAllen", "McHenry", "Medford", "Melbourne", "Memphis", "Merced", "Mesa", "Mesquite", "Miami", "Milwaukee", "Minneapolis", "Miramar", "Mission Viejo", "Mobile", "Modesto", "Monroe", "Monterey", "Montgomery", "Moreno Valley", "Murfreesboro", "Murrieta", "Muskegon", "Myrtle Beach", "Naperville", "Naples", "Nashua", "Nashville", "New Bedford", "New Haven", "New London", "New Orleans", "New York", "New York City", "Newark", "Newburgh", "Newport News", "Norfolk", "Normal", "Norman", "North Charleston", "North Las Vegas", "North Port", "Norwalk", "Norwich", "Oakland", "Ocala", "Oceanside", "Odessa", "Ogden", "Oklahoma City", "Olathe", "Olympia", "Omaha", "Ontario", "Orange", "Orem", "Orlando", "Overland Park", "Oxnard", "Palm Bay", "Palm Springs", "Palmdale", "Panama City", "Pasadena", "Paterson", "Pembroke Pines", "Pensacola", "Peoria", "Philadelphia", "Phoenix", "Pittsburgh", "Plano", "Pomona", "Pompano Beach", "Port Arthur", "Port Orange", "Port Saint Lucie", "Port St. Lucie", "Portland", "Portsmouth", "Poughkeepsie", "Providence", "Provo", "Pueblo", "Punta Gorda", "Racine", "Raleigh", "Rancho Cucamonga", "Reading", "Redding", "Reno", "Richland", "Richmond", "Richmond County", "Riverside", "Roanoke", "Rochester", "Rockford", "Roseville", "Round Lake Beach", "Sacramento", "Saginaw", "Saint Louis", "Saint Paul", "Saint Petersburg", "Salem", "Salinas", "Salt Lake City", "San Antonio", "San Bernardino", "San Buenaventura", "San Diego", "San Francisco", "San Jose", "Santa Ana", "Santa Barbara", "Santa Clara", "Santa Clarita", "Santa Cruz", "Santa Maria", "Santa Rosa", "Sarasota", "Savannah", "Scottsdale", "Scranton", "Seaside", "Seattle", "Sebastian", "Shreveport", "Simi Valley", "Sioux City", "Sioux Falls", "South Bend", "South Lyon", "Spartanburg", "Spokane", "Springdale", "Springfield", "St. Louis", "St. Paul", "St. Petersburg", "Stamford", "Sterling Heights", "Stockton", "Sunnyvale", "Syracuse", "Tacoma", "Tallahassee", "Tampa", "Temecula", "Tempe", "Thornton", "Thousand Oaks", "Toledo", "Topeka", "Torrance", "Trenton", "Tucson", "Tulsa", "Tuscaloosa", "Tyler", "Utica", "Vallejo", "Vancouver", "Vero Beach", "Victorville", "Virginia Beach", "Visalia", "Waco", "Warren", "Washington", "Waterbury", "Waterloo", "West Covina", "West Valley City", "Westminster", "Wichita", "Wilmington", "Winston", "Winter Haven", "Worcester", "Yakima", "Yonkers", "York", "Youngstown"];
 
   let latitude = ''; // this changes with each step in the app (initial ip geolocation, then dining, then event)
@@ -29,6 +32,7 @@ $(document).ready(function () {
     // etc.
   }
 
+
   /* ----------------------------------------------------------------------------------- */
   // --------------------------------- BEGIN API CALLs --------------------------------- //
   /* ----------------------------------------------------------------------------------- */
@@ -52,7 +56,7 @@ $(document).ready(function () {
       // console.log('User latitude is ' + latitude); // tell us the lat // TEST CODE
       //  console.log('User longitude is ' + longitude); // tell us the long // TEST CODE
       console.log('You are in ' + city); // TEST CODE
-      $('#city').text('It looks like you are in ' + city + '.  Is this correct?'); // populates the text for location (depends on APIResponse === true)
+      $('.city').text(city); // populates the text for location (depends on APIResponse === true)
     });
   };
   // ==================== END GeoLocation API Call ====================
@@ -60,58 +64,56 @@ $(document).ready(function () {
   // ==================== BEGIN Events API Call ====================
   let callAPIEvents = function () { // Begin ajax call for events
     loaderText = 'Finding awesome events near you...';
+    keywords = '';
+
     let url = 'https://app.ticketmaster.com/discovery/v2/events.json?city=';
     const key = '1WLkNy3Qylx70A9ds5a5gXCT2PNoGeGq';
 
-    classifications = '';
-
     for (i = 0; i < params.length; i++) {
-      let newClassification = params[i] + '&';
-      classifications = classifications + newClassification;
-      console.log(newClassification);
+      let keywordNew = params[i] + '&';
+      keywords = keywords + keywordNew;
+      console.log(keywordNew);
     };
 
     // below prevents the events from failing by searching for all events if params is empoty
     if (params.length === 0) {
-      classifications = 'all&';
+      keywords = 'all&';
     };
 
-    console.log(classifications); // TEST CODE
+    console.log(keywords); // TEST CODE
 
     // ===== TESTING CODE =====
 
-    $('#paramsList').empty(); // empty out search results from previous search
+    // $('#paramsList').empty(); // empty out search results from previous search
 
-    let queryURL = url + city + '&keyword=' + classifications + 'apikey=' + key; // city pulled from geolocatin API call (this could be moved to be called on load for efficeincy instead of tied to a click event)
+    let queryURL = url + city + '&keyword=' + keywords + 'apikey=' + key; // city pulled from geolocatin API call (this could be moved to be called on load for efficeincy instead of tied to a click event)
     $.ajax({
       url: queryURL,
       method: "GET",
+      success: function (response) {
+        eventsResponse = response;
+      }
       // dataType: 'jsonp', // optional - needed to work around a CORS warning/error
     }).then(function (response) {
+
       console.log(response); // returns the initial ip address geolocation data // TEST CODE 
       $('#eventsList').empty();
       // ===== Begin Event List Rendering =====
       // NOTE: Most of the event details should be rendered in a modal - show only the key data (event, time, date, etc.) in the list items
       for (i = 0; i < response._embedded.events.length; i++) {
-        // console.log(response._embedded.events[i].name); // TEST CODE
-
         // ===== Begin Event Item core data (main data like name, date, etc.) =====
         let event = response._embedded.events[i].name; // get event name
         let eventItem = $('<button>');
-        eventItem.addClass('col s12 l3');
-        // eventItem.addClass('waves-effect waves-light btn');
+        eventItem.addClass('col s12 l2');
         eventItem.addClass('eventCard');
         eventItem.text(event);
-        $('#eventsList').append(eventItem);
+        eventItem.attr("data-event", i);
+        let modalTrigger = $('<a>');
+        modalTrigger.addClass('modal-trigger');
+        modalTrigger.attr('href', '#modal1');
+        modalTrigger.append(eventItem)
+        $('#eventsList').append(modalTrigger);
         // ===== End Event Item core data (main data like name, date, etc.) =====
-
-        // ===== Begin Event Details core data (extra data for modals) =====
-        // let image = response._embedded.events[i].images[5].url; // get event img 
-        // let imageEvent = $('<img>');
-        // imageEvent.attr('src', image);
-        // imageEvent.addClass('image');
-        // $('#eventsList').append(imageEvent); // this may be swapped (we may not need images) or added to a seperate element that popualates on event details modal
-        // ===== End Event Details core data (extra data for modals) =====
       } // ===== End Event List Rendering =====
     }); // ===== End ajax .then actions
   };
@@ -119,16 +121,16 @@ $(document).ready(function () {
 
   // ==================== BEGIN Yelp API Call ====================
   // ==================== Yelp variables ====================
-
   let yelp = function () {
     loaderText = 'Finding tasty food near your event...';
     let category = "pizza";
-    let location = "30308";
+    // let location = "30308";
     let price = "2";
-    const radius = "1000";
+    const radius = "10000";
     let yelpApiKey = "4YFPvbnRG_3T1cP_B2tCpTFPpKnV2tgIvSmz926QynUmbZFl_y3eNsVBWjZLTNqx8y5Lth__B95rWD5_-iU0BF4Mpk9Dqz7LhB8gOq-ekL0guI0Wm1MQHX4jSUK2XHYx";
 
-    let yelpQueryURL = `https://cors-anywhere.herokuapp.com/https://api.yelp.com/v3/businesses/search?term=${category}&location=${location}&price=${price}&radius=${radius}`;
+    //let yelpQueryURL = `https://cors-anywhere.herokuapp.com/https://api.yelp.com/v3/businesses/search?term=${category}&location=${location}&price=${price}&radius=${radius}`;
+    let yelpQueryURL = `https://cors-anywhere.herokuapp.com/https://api.yelp.com/v3/businesses/search?term=${category}&latitude=${latitude}&longitude=${longitude}&price=${price}&radius=${radius}`;
     console.log(yelpQueryURL)
 
     $.ajax({
@@ -144,7 +146,6 @@ $(document).ready(function () {
     });
   }
 
-
   // ==================== END Yelp API Call ====================
 
   /* ----------------------------------------------------------------------------------- */
@@ -158,26 +159,47 @@ $(document).ready(function () {
   /* ----------------------------------------------------------------------------------- */
 
   // ==================== BEGIN - CLICK EVENT - start app - populate ip-based geo location ====================
-  $('#start').on('click', function () {
-    // $('.overlay').removeClass('hidden'); // TEST CODE
+  $('#btn-start').on('click', function () {
     callAPIGeo(); // gets the initial geolocation data for the user
-    if (APIResponse === true) {
-      // $('.overlay').addClass('hidden'); // TEST CODE
-      // $('.menu1-btn').addClass('scale-in'); // reveals the next menu options (these will eventually be in a seperate component)
-    };
+    $('#v1').css('display', 'none');
   });
   // ==================== END - CLICK EVENT - start app - populate ip-based geo location ====================
 
   // ---------- Click Event - Location Confirmation
-  $('#locationYes').on('click', function () {
+  $('#btn-city-yes').on('click', function () {
     console.log('User confirmed - update view...');
+    $('#v2-city').addClass('scale-out');
+    // $('#v2-city').removeClass('view');
+    // $('#v2-city').removeClass('vh100');
+    setTimeout(function() {
+      $('#v2-city').addClass('display-none');
+    },50);
+    
+    $('#v3').css('opacity', '1');
+    $('#v2').css('min-height', '0');
+
     // this is going to change our view here
   })
 
-  $('#locationNo').on('click', function () {
-    // displays the hidden form to update the city
-    $('#cityForm').css('display', 'block');
-  })
+function city_opt1_hide() {
+ // hides the original options
+ $('#city-opt1').addClass('scale-out')
+ // reveals the secondary options
+ $('#city-opt2').addClass('scale-in');
+}
+
+  $('#btn-city-no').on('click', city_opt1_hide);
+
+  function city_opt2_hide() {
+ // hides the seondary options
+ $('#city-opt2').removeClass('scale-in')
+ // reveals the original options
+ $('#city-opt1').removeClass('scale-out');
+  };
+
+  $('#btn-city-stay').on('click', city_opt2_hide);
+
+  // city-error
 
   // ---------- Click Event - Events API Call
   $('#eventSearch').on('click', function () {
@@ -214,26 +236,53 @@ $(document).ready(function () {
     console.log('events params: ' + params);  // TEST Code
   });
 
-
-
-  $('#locationUpdate').on('click', function () {
-    cityCheck = $('#cityNew').val();
+  $('#btn-city-change').on('click', function () {
+    cityCheck = $('#city-input').val();
     cityCheck = cityCheck.charAt(0).toUpperCase() + cityCheck.slice(1);
-    console.log(cityCheck);
     if (cityList.indexOf(cityCheck) === -1) {
-      console.log('invalid city');
-      
+      $('#city-error').removeClass('hidden');
+      console.log(cityCheck + ' is an invalid city');
     } else {
-      city = cityCheck // not right
-    }
-
+      city = cityCheck;
+      $('#city-error').addClass('hidden');
+      city_opt2_hide();
       
+    };
     console.log(city);
-    // $('#cityForm').css('display', 'none');
-    $('#city').text('It looks like you are in ' + city + '.  Is this correct?');
+    $('.city').text(city);
   })
 
-  yelp();
+  $(document.body).on('click', '.eventCard', function () {
+    a = $(this).attr('data-event');
+    console.log(a);
+    name = eventsResponse._embedded.events[a].name;
+    img = eventsResponse._embedded.events[a].images[5].url;
+    venue = eventsResponse._embedded.events[a]._embedded.venues[0].name;
+    address = eventsResponse._embedded.events[a]._embedded.venues[0].address.line1 + ' ' + eventsResponse._embedded.events[a]._embedded.venues[0].city.name + ', ' + eventsResponse._embedded.events[a]._embedded.venues[0].state.name;
+    date = eventsResponse._embedded.events[a].dates.start.localDate;
+    url = eventsResponse._embedded.events[a].url
+    eventsResponsePosition = $(this).attr('data-event');
+
+    // load the event details into the modal
+    $('#eventName').text(name);
+    $('#eventIMG').attr('src', img);
+    $('#eventVenue').text(venue);
+    $('#eventDate').text(date);
+    $('#eventAddress').text(address);
+    $('#tix').attr('href', url);
+  });
+
+  $('#eventSelect').on('click', function () {
+    latitude = eventsResponse._embedded.events[eventsResponsePosition]._embedded.venues[0].location.latitude;
+    console.log(latitude);
+    longitude = eventsResponse._embedded.events[eventsResponsePosition]._embedded.venues[0].location.longitude;
+    console.log(longitude);
+  });
+
+  $('#yelp').on('click', function () {
+    yelp();
+  })
+
   /* ----------------------------------------------------------------------------------- */
   // ------------------------------- END EVENT LISTENERS ------------------------------- //
   /* ----------------------------------------------------------------------------------- */
