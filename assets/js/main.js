d0e9a47ae6149149c05af826bb1c3e75f2cd33e1
John Robertson <tiktaalikdesign@gmail.com>
2019-04-20 10:08:18 -0400

    hecking finished

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,22 +1,46 @@
-
+let eventsResponse = '';
+let restResponse = '';
+let eventsResponsePosition = 0;
+let restResponsePosition = 0;
 
 $(document).ready(function () {
 
   // array of cities for validation
   let cityList = ["Stankonia", "Aberdeen", "Abilene", "Akron", "Albany", "Albuquerque", "Alexandria", "Allentown", "Amarillo", "Anaheim", "Anchorage", "Ann Arbor", "Antioch", "Apple Valley", "Appleton", "Arlington", "Arvada", "Asheville", "Athens", "Atlanta", "Atlantic City", "Augusta", "Aurora", "Austin", "Bakersfield", "Baltimore", "Barnstable", "Baton Rouge", "Beaumont", "Bel Air", "Bellevue", "Berkeley", "Bethlehem", "Billings", "Birmingham", "Bloomington", "Boise", "Boise City", "Bonita Springs", "Boston", "Boulder", "Bradenton", "Bremerton", "Bridgeport", "Brighton", "Brownsville", "Bryan", "Buffalo", "Burbank", "Burlington", "Cambridge", "Canton", "Cape Coral", "Carrollton", "Cary", "Cathedral City", "Cedar Rapids", "Champaign", "Chandler", "Charleston", "Charlotte", "Chattanooga", "Chesapeake", "Chicago", "Chula Vista", "Cincinnati", "Clarke County", "Clarksville", "Clearwater", "Cleveland", "College Station", "Colorado Springs", "Columbia", "Columbus", "Concord", "Coral Springs", "Corona", "Corpus Christi", "Costa Mesa", "Dallas", "Daly City", "Danbury", "Davenport", "Davidson County", "Dayton", "Daytona Beach", "Deltona", "Denton", "Denver", "Des Moines", "Detroit", "Downey", "Duluth", "Durham", "El Monte", "El Paso", "Elizabeth", "Elk Grove", "Elkhart", "Erie", "Escondido", "Eugene", "Evansville", "Fairfield", "Fargo", "Fayetteville", "Fitchburg", "Flint", "Fontana", "Fort Collins", "Fort Lauderdale", "Fort Smith", "Fort Walton Beach", "Fort Wayne", "Fort Worth", "Frederick", "Fremont", "Fresno", "Fullerton", "Gainesville", "Garden Grove", "Garland", "Gastonia", "Gilbert", "Glendale", "Grand Prairie", "Grand Rapids", "Grayslake", "Green Bay", "GreenBay", "Greensboro", "Greenville", "Gulfport-Biloxi", "Hagerstown", "Hampton", "Harlingen", "Harrisburg", "Hartford", "Havre de Grace", "Hayward", "Hemet", "Henderson", "Hesperia", "Hialeah", "Hickory", "High Point", "Hollywood", "Honolulu", "Houma", "Houston", "Howell", "Huntington", "Huntington Beach", "Huntsville", "Independence", "Indianapolis", "Inglewood", "Irvine", "Irving", "Jackson", "Jacksonville", "Jefferson", "Jersey City", "Johnson City", "Joliet", "Kailua", "Kalamazoo", "Kaneohe", "Kansas City", "Kennewick", "Kenosha", "Killeen", "Kissimmee", "Knoxville", "Lacey", "Lafayette", "Lake Charles", "Lakeland", "Lakewood", "Lancaster", "Lansing", "Laredo", "Las Cruces", "Las Vegas", "Layton", "Leominster", "Lewisville", "Lexington", "Lincoln", "Little Rock", "Long Beach", "Lorain", "Los Angeles", "Louisville", "Lowell", "Lubbock", "Macon", "Madison", "Manchester", "Marina", "Marysville", "McAllen", "McHenry", "Medford", "Melbourne", "Memphis", "Merced", "Mesa", "Mesquite", "Miami", "Milwaukee", "Minneapolis", "Miramar", "Mission Viejo", "Mobile", "Modesto", "Monroe", "Monterey", "Montgomery", "Moreno Valley", "Murfreesboro", "Murrieta", "Muskegon", "Myrtle Beach", "Naperville", "Naples", "Nashua", "Nashville", "New Bedford", "New Haven", "New London", "New Orleans", "New York", "New York City", "Newark", "Newburgh", "Newport News", "Norfolk", "Normal", "Norman", "North Charleston", "North Las Vegas", "North Port", "Norwalk", "Norwich", "Oakland", "Ocala", "Oceanside", "Odessa", "Ogden", "Oklahoma City", "Olathe", "Olympia", "Omaha", "Ontario", "Orange", "Orem", "Orlando", "Overland Park", "Oxnard", "Palm Bay", "Palm Springs", "Palmdale", "Panama City", "Pasadena", "Paterson", "Pembroke Pines", "Pensacola", "Peoria", "Philadelphia", "Phoenix", "Pittsburgh", "Plano", "Pomona", "Pompano Beach", "Port Arthur", "Port Orange", "Port Saint Lucie", "Port St. Lucie", "Portland", "Portsmouth", "Poughkeepsie", "Providence", "Provo", "Pueblo", "Punta Gorda", "Racine", "Raleigh", "Rancho Cucamonga", "Reading", "Redding", "Reno", "Richland", "Richmond", "Richmond County", "Riverside", "Roanoke", "Rochester", "Rockford", "Roseville", "Round Lake Beach", "Sacramento", "Saginaw", "Saint Louis", "Saint Paul", "Saint Petersburg", "Salem", "Salinas", "Salt Lake City", "San Antonio", "San Bernardino", "San Buenaventura", "San Diego", "San Francisco", "San Jose", "Santa Ana", "Santa Barbara", "Santa Clara", "Santa Clarita", "Santa Cruz", "Santa Maria", "Santa Rosa", "Sarasota", "Savannah", "Scottsdale", "Scranton", "Seaside", "Seattle", "Sebastian", "Shreveport", "Simi Valley", "Sioux City", "Sioux Falls", "South Bend", "South Lyon", "Spartanburg", "Spokane", "Springdale", "Springfield", "St. Louis", "St. Paul", "St. Petersburg", "Stamford", "Sterling Heights", "Stockton", "Sunnyvale", "Syracuse", "Tacoma", "Tallahassee", "Tampa", "Temecula", "Tempe", "Thornton", "Thousand Oaks", "Toledo", "Topeka", "Torrance", "Trenton", "Tucson", "Tulsa", "Tuscaloosa", "Tyler", "Utica", "Vallejo", "Vancouver", "Vero Beach", "Victorville", "Virginia Beach", "Visalia", "Waco", "Warren", "Washington", "Waterbury", "Waterloo", "West Covina", "West Valley City", "Westminster", "Wichita", "Wilmington", "Winston", "Winter Haven", "Worcester", "Yakima", "Yonkers", "York", "Youngstown"];
-  let backgroundIMG = [''];
+
   // Declaring our 'global' variables
   let latitude = ''; // event latitude to feed to yelp
   let longitude = ''; // event longitude to feed to yelp
   let city = ''; // holds the city for dynamic text rendering
-  eventKeyword = '' // keyword for event search
+  let eventKeyword = '' // keyword for event search
   let loaderText = ''; // changes with each API call to give user feedback
-  let eventsResponse = '';
-  let restResponse = '';
-  let eventsResponsePosition = 0;
-  let restResponsePosition = 0;
 
-  $('.modal').modal();
+
+  $('.modal').modal(); // intializing modals
+
+  /* ----------------------------------------------------------------------------------- */
+  // --------------------------------- FIRE FIRE FIRE ---------------------------------- //
+  /* ----------------------------------------------------------------------------------- */
+  var config = {
+    apiKey: "AIzaSyDcy7crIuPJYtdVRwFQFb6Vyp5vPlZrgAE",
+    authDomain: "group-project-1-12e16.firebaseapp.com",
+    databaseURL: "https://group-project-1-12e16.firebaseio.com",
+    projectId: "group-project-1-12e16",
+    storageBucket: "group-project-1-12e16.appspot.com",
+    messagingSenderId: "5760922749"
+  };
+
+  firebase.initializeApp(config);
+
+  let database = firebase.database();
+
+  console.log(firebase);
+
+  var ref = database.ref('rest')
+
+  /* ------------------------------------------------------------------------------------- */
+  // --------------------------------- BASE BASE BASE ------------------------------------ //
+  /* ------------------------------------------------------------------------------------- */
 
   /* ----------------------------------------------------------------------------------- */
   // --------------------------------- BEGIN API CALLs --------------------------------- //
@@ -193,8 +217,6 @@ $(document).ready(function () {
   // ---------------------------------- END API CALLs ---------------------------------- //
   /* ----------------------------------------------------------------------------------- */
 
-
-
   /* ----------------------------------------------------------------------------------- */
   // ------------------------------ BEGIN EVENT LISTENERS ------------------------------ //
   /* ----------------------------------------------------------------------------------- */
@@ -211,7 +233,7 @@ $(document).ready(function () {
   });
   // ==================== END - CLICK EVENT - start app - populate ip-based geo location ====================
 
-  // click event - location confirmation
+  // ==================== BEGIN - CLICK EVENT - VIEW CHANGE - update view 2 > 3 ====================
   $('#btn-city-yes').on('click', function () {
 
     $('#v2-city').addClass('scale-out');
@@ -227,7 +249,9 @@ $(document).ready(function () {
     console.log('- - - - - - - - - - - - - - - - - - - - ');
     console.log('View updated - user city confirmed');
   });
+  // ==================== END - CLICK EVENT - VIEW CHANGE - update view 2 > 3 ====================
 
+  // ==================== BEGIN - CLICK EVENT - update city functionality ====================
   function city_opt1_hide() {
     $('#city-opt1').addClass('scale-out') // hides the original options
     $('#city-opt2').addClass('scale-in'); // reveals the secondary options
@@ -238,44 +262,11 @@ $(document).ready(function () {
     $('#city-opt1').removeClass('scale-out'); // reveals the original options
   };
 
-  // click event to reveal city update options
-  $('#btn-city-no').on('click', city_opt1_hide);
+  $('#btn-city-no').on('click', city_opt1_hide); // click event to reveal city update options
 
-  // click event to revert to original city options
-  $('#btn-city-stay').on('click', city_opt2_hide);
+  $('#btn-city-stay').on('click', city_opt2_hide); // click event to revert to original city options
   $('#btn-city-stay').on('click', function () {
     $('#city-error').addClass('hidden');
-  });
-
-
-  // ========== adding and removing event search parameters from eventKeyword array ========== //
-  $('.btn-events').on('click', function () {
-
-    eventKeyword = $(this).text(); // sets keyword for event search
-
-    console.log('- - - - - - - - - - - - - - - - - - - - ');
-    console.log('User event search: ' + eventKeyword);
-
-    callAPIEvents(); // searchs events by keyword 
-
-    // ========== Begin Events Keyword Parameters for future versions ========== //
-    // This code allows for the addition of multiple events keyword functionality in a future version
-
-    // param = $(this).text(); // gets button text
-    // let paramIndex = eventKeyword.indexOf(param); // gets index position of this event param
-    // if ($(this).hasClass('btn-events-active')) { // checks to see if added
-    //   $(this).removeClass('btn-events-active'); // removes if in eventKeyword array
-    //   $(this).css('background', 'white');
-    //   $(this).css('color', 'black');
-    //   eventKeyword.splice(paramIndex, 1);
-    // } else {
-    //   $(this).addClass('btn-events-active'); // adds active class to this
-    //   $(this).css('background', 'lightseagreen');
-    //   $(this).css('color', 'white');
-    //   eventKeyword.push($(this).text()) // adds to eventKeyword array
-    // };
-    // console.log('events eventKeyword: ' + eventKeyword);  // TEST Code
-    // ========== End Events Keyword Parameters for future versions ========== //
   });
 
   // updating the city based on user input
@@ -305,7 +296,41 @@ $(document).ready(function () {
     console.log('- - - - - - - - - - - - - - - - - - - - ');
     console.log('Current city - ' + city);
   })
+  // ==================== END - CLICK EVENT - update city functionality ====================
 
+  // ========== BEGIN - CLICK EVENT - select event search parameters and call events API ========== 
+  $('.btn-events').on('click', function () {
+
+    eventKeyword = $(this).text(); // sets keyword for event search
+
+    console.log('- - - - - - - - - - - - - - - - - - - - ');
+    console.log('User event search: ' + eventKeyword);
+
+    callAPIEvents(); // searchs events by keyword 
+
+    // ========== Begin Events Keyword Parameters for future versions ========== //
+    // This code allows for the addition of multiple events keyword functionality in a future version
+
+    // param = $(this).text(); // gets button text
+    // let paramIndex = eventKeyword.indexOf(param); // gets index position of this event param
+    // if ($(this).hasClass('btn-events-active')) { // checks to see if added
+    //   $(this).removeClass('btn-events-active'); // removes if in eventKeyword array
+    //   $(this).css('background', 'white');
+    //   $(this).css('color', 'black');
+    //   eventKeyword.splice(paramIndex, 1);
+    // } else {
+    //   $(this).addClass('btn-events-active'); // adds active class to this
+    //   $(this).css('background', 'lightseagreen');
+    //   $(this).css('color', 'white');
+    //   eventKeyword.push($(this).text()) // adds to eventKeyword array
+    // };
+    // console.log('events eventKeyword: ' + eventKeyword);  // TEST Code
+    // ========== End Events Keyword Parameters for future versions ========== //
+  });
+
+  // ========== END - CLICK EVENT - select event search parameters and call events API ========== 
+
+  // ========== BEGIN - CLICK EVENT - Event Modal and Event Selection ========== 
   // click event for dynamically rendered events
   $(document.body).on('click', '.eventCard', function () {
 
@@ -355,9 +380,9 @@ $(document).ready(function () {
     console.log('- - - - - - - - - - - - - - - - - - - - ');
     console.log('View updated - location passed to Yelp API');
   });
+  // ========== END - CLICK EVENT - Event Modal and Event Selection ========== 
 
-  // ----------------------------------------------------------------------------------------------------------------
-
+  // ========== BEGIN - CLICK EVENT - Restaurant Modal and Restaurant Selection ========== 
   // click event for dynamically rendered restaurants
   $(document.body).on('click', '.restCard', function () {
 
@@ -407,7 +432,14 @@ $(document).ready(function () {
     console.log('Restaurant selection confirmed - ' + restResponse.businesses[a].name);
     console.log('- - - - - - - - - - - - - - - - - - - - ');
     console.log('Updating view to Summary.');
+
+    data = restResponse.businesses[restResponsePosition]; // set restaurant to var for firebase
+  
+    ref.push(data); // push to firebase
+
+    console.log(data);
   });
+  // ========== BEGIN - CLICK EVENT - Restaurant Modal and Restaurant Selection ========== 
 
   /* ----------------------------------------------------------------------------------- */
   // ------------------------------- END EVENT LISTENERS ------------------------------- //
